a848b3bd667c43a0bb46858eeb2c42e2f0748505
jesse-dev09 <komarik9955@gmail.com>
2026-05-03 21:24:08 +0500

    Update first_week_mission.py

--- first_week_mission.py
+++ first_week_mission.py
@@ -2,7 +2,7 @@
 user_name = input("What is your name?")
 print("Hello, " + user_name + "!")
 age_text = input("How old are you?")
-age_number = int(age_text) # Превратили текст в число!
+age_number = int(age_text) 
 сity_text = input("What city do you live in?")
 birth_year = 2026 - age_number
 print(f"Hello, {user_name}! You are {age_number} years old and you live in {city_text}. So you were born around {birth_year}.")
